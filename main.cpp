ac4c1199441ad19f59e88932de9131445b73e5e7
Samuel Kroiß <semmikroiss@gmail.com>
2022-12-23 18:04:47 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+// Source: https://youtu.be/jUZZC9UXyFs
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char* argv[])
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Hello SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if(window == NULL)
+    {
+        std::cout << "Could not create Window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+
+    while(true)
+    {
+        if(SDL_PollEvent(&windowEvent))
+        {
+            if(windowEvent.type == SDL_QUIT)
+            {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
